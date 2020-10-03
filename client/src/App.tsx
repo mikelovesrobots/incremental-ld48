57f17836421a6f8e182367c0a1500b200e1a6bcb
Mike Judge <mikelovesrobots@gmail.com>
2020-10-03 13:33:54 -0700

    Add a bar for keeping track of the lich's power

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import Container from 'react-bootstrap/esm/Container';
 import Jumbotron from 'react-bootstrap/esm/Jumbotron';
+import Navbar from 'react-bootstrap/esm/Navbar';
 
 const App: React.FunctionComponent = () => (
   <Container className="p-3">
     <Jumbotron>
       <h1 className="header">Infinitely Worse - A Lich Adventure</h1>
     </Jumbotron>
+    <Navbar variant="dark" bg="info" sticky="top">
+      <Navbar.Text>
+        <span className="text-uppercase font-weight-bold">Power:</span> 100
+      </Navbar.Text>
+    </Navbar>
   </Container>
 );
 
