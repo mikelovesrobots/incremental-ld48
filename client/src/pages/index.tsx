354a93a1fa90e2a87c5eecc88e26fe415e241a55
Mike Judge <mikelovesrobots@gmail.com>
2020-10-02 23:07:10 -0700

    Lets increment some numbers

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,17 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "components/layout"
 import { typography } from "styles"
 import styled from "@emotion/styled"
+import { useInterval } from "beautiful-react-hooks"
 
 const Headline = styled.h1({ ...typography.headline1 })
 const Profits = styled.p({ ...typography.body })
 
-const cash = 10
-const IndexPage: React.FunctionComponent = () => (
-  <Layout>
-    <Headline>Infinitely Worse - An Adventure</Headline>
-    <Profits>Cash: {cash}</Profits>
-  </Layout>
-)
+const IndexPage: React.FunctionComponent = () => {
+  const [cash, setCash] = useState(0)
+
+  useInterval(() => {
+    setCash(cash + 1)
+  }, 100)
+
+  return (
+    <Layout>
+      <Headline>Infinitely Worse - An Adventure</Headline>
+      <Profits>Cash: {cash.toExponential(1)}</Profits>
+    </Layout>
+  )
+}
 
 export default IndexPage
