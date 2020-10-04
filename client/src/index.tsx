85d1c50d51b2919400a610fc2f1bcb81fdbd6925
Mike Judge <mikelovesrobots@gmail.com>
2020-10-04 09:51:52 -0700

    Fix the page title

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Helmet from 'react-helmet';
 import './index.css';
 import 'bootswatch/dist/cyborg/bootstrap.min.css';
 import App from './App';
@@ -7,6 +8,14 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Infinitely Worse</title>
+      <link
+        rel="canonical"
+        href="https://mikelovesrobots.github.io/incremental-ld48/"
+      />
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
